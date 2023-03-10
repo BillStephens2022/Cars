61827e2172126afc12c5798ba902df28cfbc1071
BillStephens2022 <two4onebill@yahoo.com>
2023-03-10 11:16:01 -0500

    added functionality to search for a car

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -1,10 +1,12 @@
-import { useSelector, useDispatch } from 'react-redux';
-import { removeCar } from '../store';
+import { useSelector, useDispatch } from "react-redux";
+import { removeCar } from "../store";
 
 function CarList() {
   const dispatch = useDispatch();
-  const cars = useSelector((state) => {
-    return state.cars.data;
+  const cars = useSelector(({ cars: { data, searchTerm } }) => {
+    return data.filter((car) =>
+      car.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
   });
 
   const handleCarDelete = (car) => {
